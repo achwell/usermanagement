7284c42171482ab0929d89639d4c9ab8cd1d8933
Axel Wulff Sæther <axelwulff@mac.com>
2021-06-28 11:38:56 +0200

    Spring 2.5.2

--- usermanagementbackend/src/main/java/net/axelwulff/usermanagement/exception/ExceptionHandling.java
+++ usermanagementbackend/src/main/java/net/axelwulff/usermanagement/exception/ExceptionHandling.java
@@ -131,16 +131,4 @@ public class ExceptionHandling implements ErrorController {
     private ResponseEntity<HttpResponse> createHttpResponse(HttpStatus httpStatus, String message) {
         return new ResponseEntity<>(new HttpResponse(httpStatus, message), httpStatus);
     }
-
-    /**
-     * The return value from this method is not used; the property `server.error.path`
-     * must be set to override the default error page path.
-     *
-     * @return the error path
-     * @deprecated since 2.3.0 in favor of setting the property `server.error.path`
-     */
-    @Override
-    public String getErrorPath() {
-        return ERROR_PATH;
-    }
 }
